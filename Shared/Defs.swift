3be473f1b752a2f4c4e1bba0ff070fab8529d665
W-Mai <1341398182@qq.com>
2021-09-18 15:55:55 +0800

    添加文件结构信息的注释👀

--- Shared/Defs.swift
+++ Shared/Defs.swift
@@ -6,22 +6,26 @@
 //
 import SwiftUI
 
+// 任务状态
 enum TaskStatus: String, Codable {
     case finished = "FINISHED",
          original = "ORIGINAL",
          todo = "TODO"
 }
 
+// 显示模式
 enum DisplayMode: String, Codable {
-    case FullSquareMode
-    case SimpleProcessBarMode
+    case FullSquareMode // 方形样式
+    case SimpleProcessBarMode // 线条进度样式
 }
 
+// 显示类别
 enum DisplayCatagory: String, Codable {
-    case All
-    case Todos
+    case All    // 显示所有任务
+    case Todos  // 只显示待办事项
 }
 
+// 文件结构版本号
 struct FileFormatVersion : Codable, Comparable{
     var a, b, c : Int
     
@@ -30,6 +34,7 @@ struct FileFormatVersion : Codable, Comparable{
     }
 }
 
+// 任务状态改变记录，记录任务状态改变情况，方便查看每日完成情况
 struct TaskStatusChangeRecord : Codable {
     var taskId : UUID
     var changeDate : Date
@@ -38,45 +43,50 @@ struct TaskStatusChangeRecord : Codable {
     var extra : String?
 }
 
+// 任务详情
 struct TaskInfo: Codable {
-    var name: String
-    var content : String
-    var status: TaskStatus
-    var createDate : Date
+    var name: String        // 任务名
+    var content : String    // 内容
+    var status: TaskStatus  // 任务状态
+    var createDate : Date   // 创建日期
     
     var id : UUID
     var extra : String?
 }
 
+// 项目详情
 struct ProjectInfo: Codable {
-    var name : String
-    var tasks : [TaskInfo]
+    var name : String       // 项目名称
+    var tasks : [TaskInfo]  // 所包含的任务
     
     var id : UUID
     var extra : String?
 }
 
+// 项目组详情
 struct ProjectGroupInfo: Codable {
-    var name : String
-    var projects : [ProjectInfo]
+    var name : String               // 项目组名称
+    var projects : [ProjectInfo]    // 所包含的项目
     
     var id : UUID
     var extra : String?
 }
 
+// 文件信息详情
 struct FileInfos : Codable {
-    var documentVersion : FileFormatVersion
-    var createDate: Date
-    var author : String
-    var displayMode : DisplayMode
-    var displayCatagory : DisplayCatagory
+    var documentVersion : FileFormatVersion // 文件格式版本
+    var createDate: Date                    // 文件创建日期
+    var author : String                     // 作者
+    var displayMode : DisplayMode           // 显示模式
+    var displayCatagory : DisplayCatagory   // 显示类别
     
     var extra : String?
 }
 
+// 文件结构
 struct PlannerFileStruct: Codable {
-    var fileInformations : FileInfos
+    var fileInformations : FileInfos                    // 文件信息
     
-    var projectGroups : [ProjectGroupInfo]
-    var taskStatusChanges : [TaskStatusChangeRecord]
+    var projectGroups : [ProjectGroupInfo]              // 所包含的项目组
+    var taskStatusChanges : [TaskStatusChangeRecord]    // 任务改变状态
 }
