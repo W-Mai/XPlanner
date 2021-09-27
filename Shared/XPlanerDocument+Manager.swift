94f65933dc5595f9977f7666b8205c88e0406a7c
W-Mai <1341398182@qq.com>
2021-09-27 18:52:51 +0800

    增加：添加组，删除组，添加项目，删除项目

--- Shared/XPlanerDocument+Manager.swift
+++ Shared/XPlanerDocument+Manager.swift
@@ -9,18 +9,61 @@ import Foundation
 import SwiftUI
 
 extension XPlanerDocument {
-    func add(_ undoManager : UndoManager?) {
+    func addGroup(nameIs grpName : String , _ undoManager : UndoManager?) {
         plannerData.projectGroups.append(
             ProjectGroupInfo(
-            name: Date().description,
-            projects: [ProjectInfo](),
-            id: UUID()
-        ))
+                name: grpName,
+                projects: [ProjectInfo]()
+            ))
         
         undoManager?.registerUndo(withTarget: self, handler: { doc in
             doc.plannerData.projectGroups.removeLast()
         })
         
+    }
+    
+    func removeGroup(idIs grpId : UUID, _ undoManager : UndoManager?){
+        guard let index = plannerData.projectGroups.firstIndex (where: { grp in
+            grp.id == grpId
+        }) else { return }
+        
+        let old_data = plannerData.projectGroups[index]
+        
+        plannerData.projectGroups.remove(at: index)
+        
+        undoManager?.registerUndo(withTarget: self, handler: { doc in
+            doc.plannerData.projectGroups.insert(old_data, at: index)
+        })
+    }
+    
+    func addProject(nameIs prjName : String , for grpId : UUID, _ undoManager : UndoManager?) {
+        guard let index = plannerData.projectGroups.firstIndex (where: { grp in
+            grp.id == grpId
+        }) else { return }
+        plannerData.projectGroups[index].projects.append(ProjectInfo(name: prjName, tasks: [TaskInfo]()))
+        
+        undoManager?.registerUndo(withTarget: self, handler: { doc in
+            doc.plannerData.projectGroups[index].projects.removeLast()
+        })
+        
+    }
+    
+    func removeProject(idIs prjId : UUID, from grpId : UUID, _ undoManager : UndoManager?){
+        guard let index = plannerData.projectGroups.firstIndex (where: { grp in
+            grp.id == grpId
+        }) else { return }
+        
+        guard let indexPrj = plannerData.projectGroups[index].projects.firstIndex (where: { prj in
+            prj.id == prjId
+        }) else { return }
+        
+        let old_data = plannerData.projectGroups[index].projects[indexPrj]
+        
+        plannerData.projectGroups[index].projects.remove(at: indexPrj)
+        
+        undoManager?.registerUndo(withTarget: self, handler: { doc in
+            doc.plannerData.projectGroups[index].projects.insert(old_data, at: index)
+        })
     }
     
     func toggleDisplayMode(simple: Bool ,_ undoManager : UndoManager?){
